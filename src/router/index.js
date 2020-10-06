1728652ed9341ac67cf8a2100c0c5ae8214f7f49
wuhen <1738100406@qq.com>
2020-10-06 19:50:30 +0800

    渲染支付页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
-
+import index from '@/views/index.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,7 +9,7 @@ const routes = [
     path: '/', component: Home,
     children: [
       { path: '', redirect: ('/index') },
-      { path: '/index', component: () => import("../views/index.vue") },
+      { path: '/index', component: index },
       { path: '/order', component: () => import("../views/Order.vue") },
       { path: '/me', component: () => import("../views/Me.vue") },
       { path: '/address', name: 'address', component: () => import("../views/Address.vue") },
@@ -17,6 +17,7 @@ const routes = [
       { path: '/addaddress', name: 'addaddress', component: () => import("../views/Order/Addaddress.vue") },
       { path: '/settlement', name: 'settlement', component: () => import("../views/Order/Settlement.vue") },
       { path: '/remark', name: 'remark', component: () => import("../views/Order/Remark.vue") },
+      { path: '/pay', name: 'pay', component: () => import("../views/Order/Pay.vue") },
       { path: '/city', name: 'city', component: () => import("../views/City.vue") },
       { path: '/search', name: 'search', component: () => import("../views/Search.vue") },
       {
